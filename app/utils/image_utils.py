22b518fd49e38bce6b13ac452ed421ea9935d7f3
zamax14 <zarate.alejandroM14@live.com.mx>
2024-06-28 20:04:49 -0600

    [refactor] Moving image operations from process_image.py to utils/image_utils.py

---
+++ app/utils/image_utils.py
@@ -0,0 +1,58 @@
+import numpy as np
+from PIL import Image
+
+def yolobbox2bbox(x,y,w,h):
+    x1, y1 = x-w/2, y-h/2
+    x2, y2 = x+w/2, y+h/2
+    xyxy = np.array([x1, y1, x2, y2]).transpose()
+    return xyxy 
+
+
+def expand2square(pil_img, background_color, resize):
+    width, height = pil_img.size
+    if width == height:
+        result  = pil_img
+    elif width > height:
+        result = Image.new(pil_img.mode, (width, width), background_color)
+        result.paste(pil_img, (0, (width - height) // 2))
+    else:
+        result = Image.new(pil_img.mode, (height, height), background_color)
+        result.paste(pil_img, ((height - width) // 2, 0))
+    result = result.resize(resize)
+    return result
+
+def non_max_suppression_fast(boxes, overlapThresh):
+	# if there are no boxes, return an empty list
+	if len(boxes) == 0:
+		return []
+	if boxes.dtype.kind == "i":
+		boxes = boxes.astype("float")
+	pick = []
+
+	x1 = boxes[:,0]
+	y1 = boxes[:,1]
+	x2 = boxes[:,2]
+	y2 = boxes[:,3]
+
+	area = (x2 - x1 + 1) * (y2 - y1 + 1)
+	idxs = np.argsort(y2)
+
+	while len(idxs) > 0:
+		last = len(idxs) - 1
+		i = idxs[last]
+		pick.append(i)
+
+		xx1 = np.maximum(x1[i], x1[idxs[:last]])
+		yy1 = np.maximum(y1[i], y1[idxs[:last]])
+		xx2 = np.minimum(x2[i], x2[idxs[:last]])
+		yy2 = np.minimum(y2[i], y2[idxs[:last]])
+
+		w = np.maximum(0, xx2 - xx1 + 1)
+		h = np.maximum(0, yy2 - yy1 + 1)
+
+		overlap = (w * h) / area[idxs[:last]]
+
+		idxs = np.delete(idxs, np.concatenate(([last],
+			np.where(overlap > overlapThresh)[0])))
+
+	return boxes[pick].astype("int")
